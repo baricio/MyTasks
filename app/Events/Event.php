527b2f3913db821e75997d713748dd9494293f44
fabricio@maxhospedagem.com.br <fabricio@maxhospedagem.com.br>
2016-05-10 16:31:53 -0300

    first commit task

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace mytasks\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace mytasks;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+}
